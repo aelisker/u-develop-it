a8c2fa2958afccde7f41dbd03e87028046cc0ee7
Aaron Lisker <aelisker@gmail.com>
2020-05-25 16:33:17 -0700

    added final routes and vote post method

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 
 CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
@@ -22,5 +23,15 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL,
   party_id INTEGER UNSIGNED,
-  CONSTAINT fk_party, FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  CONSTRAINT fk_party, FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE votes (
+  id INTEGER PRIMARY KEY,
+  voter_id INTEGER UNSIGNED NOT NULL,
+  candidate_id INTEGER UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
